4984d4a14aa00a38d7383ebaaedc8c412062dfb8
DimaSmile <efremov2293@gmail.com>
2019-05-13 00:20:24 +0300

    learn swagger-magento2-client

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -22,25 +22,28 @@ class DashboardController extends Controller
 
         // $api = new ApiFactory($configuration);
         // $client = $api->getApiClient();
-        // echo '<pre>';
         // // $response = $client->request('GET', 'https://api.github.com/repos/guzzle/guzzle');
         // var_dump($client->getConfiguration());
         // return view('backend.index');
-
-
+        
+        
         $configuration = new Configuration;
-        $configuration->setHost('http://maga2.loc');
+        $configuration->setHost('http://maga2.loc/rest');
         // $configuration->setConsumerKey('f0kjk008h8sph150bx01qq6s6trpqul2');
         // $configuration->setConsumerSecret('2qafh9r865tavgx82wsq8o45i0cb00qr');
         $configuration->setUsername('test123');
         $configuration->setPassword('test123');
+        $configuration->setAccessToken('1k4df60jqlt1sil3dujys4wwn6vwxvf3');
+
         $configuration->addDefaultHeader('Content-Type', 'application/json');
+        $configuration->addDefaultHeader('Authorization', 'Bearer 5ljkcfhalf7320m3y3ehubrhxtekfcdb');
         $api = new ApiClient($configuration);
         // dd($api->callApi('rest/V1/integration/admin/token', 'POST', [], []));
         // $configuration->setAccessToken('1k4df60jqlt1sil3dujys4wwn6vwxvf3');
         // $configuration->setTokenSecret('70mv5vo3oqiprh1c68qkvowsk2icic9s');
         $api_instance = new \SpringImport\Swagger\Magento2\Client\Api\CatalogProductRepositoryV1Api($api);
-
+        echo '<pre>';
+        // var_dump($api_instance->getApiClient());exit;
         try {
             $result = $api_instance->catalogProductRepositoryV1GetGet('test');
             print_r($result);
